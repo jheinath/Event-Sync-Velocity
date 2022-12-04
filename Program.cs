728ee4a34a5eccdaebf7d3a2bd4739dfb1a1d07b
jheinath <julianheinath@gmail.com>
2022-12-04 15:55:55 +0100

    fix texts

--- Program.cs
+++ Program.cs
@@ -12,8 +12,8 @@ var totalEventCount = Console.ReadLine();
 Console.WriteLine("Please insert the current event version your syncer is at");
 var currentEventCount = Console.ReadLine();
 
-Console.WriteLine("Please insert date (dd-mm-yyyy) when you of the event snapshot");
+Console.WriteLine("Please insert date (dd-mm-yyyy)  of the event snapshot");
 var currentEventSyncDateInput = Console.ReadLine();
 
-Console.WriteLine("Please insert the time (hh-mm)<24hrs> when of the event snapshot");
+Console.WriteLine("Please insert the time (hh-mm)<24hrs> of the event snapshot");
 var currentEventSyncTimeInput = Console.ReadLine();
